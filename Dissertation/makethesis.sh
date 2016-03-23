f586782aa638c39793b588a70516850251a67644
Chris Thomson <Chris Thomson>
2016-03-23 22:27:16 +0100

    Update dissertation files

--- Dissertation/makethesis.sh
+++ Dissertation/makethesis.sh
@@ -1,18 +1,18 @@
 #/bin/bash
 cd figures; ./Makefigures; cd ..
-pdflatex main.tex --draftmode || exit 1
+pdflatex Dissertation.tex --draftmode || exit 1
 
-makeindex main.nlo -s nomencl.ist -o main.nls  || exit 1
-bibtex main  || exit 1
-for file in $(ls main*-blx.aux); do
+makeindex Dissertation.nlo -s nomencl.ist -o Dissertation.nls  || exit 1
+bibtex Dissertation  || exit 1
+for file in $(ls Dissertation*-blx.aux); do
     bibtex $file 
 done
-Words=$(pdftotext main.pdf - | wc -w)
+Words=$(pdftotext Dissertation.pdf - | wc -w)
 
-WC=$(./texcount/texcount.pl -inc -1 -sum=1,1,1 -q main.tex | gawk '{print $1}')
+WC=$(./texcount/texcount.pl -inc -1 -sum=1,1,1 -q Dissertation.tex | gawk '{print $1}')
 
-pdflatex main.tex --draftmode || exit 1
-pdflatex main.tex  || exit 1
+pdflatex Dissertation.tex --draftmode || exit 1
+pdflatex Dissertation.tex  || exit 1
 
 echo "*******************************************"
 echo "       Estimated Word Count = $WC"
